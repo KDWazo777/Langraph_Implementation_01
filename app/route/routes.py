1c162ed6877208ced81e1dad18d89bc3a4e9def0
KDWazo777 <krishnadas.wazo@gmail.com>
2026-07-23 13:01:12 +0530

    Modifying the routes.py

--- app/route/routes.py
+++ app/route/routes.py
@@ -1,16 +1,25 @@
 from fastapi import APIRouter, HTTPException
 
 from app.agents.agent import agent
+from app.agents.company_agent import company_agent
 from app.utils.schemas import TestRequest
 
 router = APIRouter(prefix="/api")
 
+def select_agent(user_input: str):
+    user_input = user_input.lower()
+
+    # Routing logic
+    if any(op in user_input for op in ["+", "-", "*", "/", "%", "**"]):
+        return agent
+
+    return company_agent
 
 @router.post("/test")
 async def test(req: TestRequest):
 
     try:
-
+        agent = select_agent(req.input)
         response = agent.invoke({"messages": [{"role": "user","content": req.input}]})
         return { "success": True,"result": response["messages"][-1].content}
 
