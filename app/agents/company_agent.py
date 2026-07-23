3229de49a867213eb65a83580709c5274c50f9ba
KDWazo777 <krishnadas.wazo@gmail.com>
2026-07-23 12:50:44 +0530

    Add copmpany_agent.py to repository

---
+++ app/agents/company_agent.py
@@ -0,0 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.prebuilt import create_react_agent
+
+from app.tools.company_tool import company_info
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0,
+)
+
+company_agent = create_react_agent(
+    model=llm,
+    tools=[company_info],
+    prompt="""
+You are a Company Assistant.
+Always use the company_info tool to answer questions
+about the company.
+Do not make up information.
+"""
+)
