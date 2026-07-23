60f6218ee13acf43046995059dceb93822d8bab0
KDWazo777 <krishnadas.wazo@gmail.com>
2026-07-23 12:57:49 +0530

    Modify agent.py to repository

--- app/agents/agent.py
+++ app/agents/agent.py
@@ -10,7 +10,7 @@ from app.tools.Cal_tool import calculator, no_result
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-flash-lite",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
     temperature=0
 )
